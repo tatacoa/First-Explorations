d2a9f9c4d97f3b8d754e47add80a3b8d36005f1b
binsi <binasteinert@hotmail.de>
2018-05-14 18:05:35 +0200

    Update DimensionReduction.R

--- DimensionReduction.R
+++ DimensionReduction.R
@@ -1,10 +1,17 @@
 ## Data Visualization
 ## DIMENSION REDUCION
 
+# install.packages("factoextra")
+library(factoextra)
+
 lab <- read.csv2("LabMeasurements-Color-Card.csv")
 
-# Run a K-means clustering with 4 clusters and plot the clusters for first 2 principal components
-km.out<-kmeans(lab,centers=3,nstart=1) #run with four clusters
+# function will determine best k via elbow method
+fviz_nbclust(lab, kmeans, method = "wss")
+# => elbow is at cluster = 3
+
+# Run a K-means clustering with 3 clusters and plot the clusters for first 2 principal components
+km.out<-kmeans(lab,centers=3,nstart=1) #run with three clusters
 pr.out<-prcomp(lab,scale=TRUE)
 plot(pr.out$x[,1:2],type="n", main="546 Clustered Color Cards", ylab="PC2")
 text(pr.out$x[,1:2],col=km.out$cluster)
