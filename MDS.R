36db621187a0e370a87aa88b1d9f703a8366ce4b
binsi <binasteinert@hotmail.de>
2018-05-14 20:06:35 +0200

    Update MDS.R

--- MDS.R
+++ MDS.R
@@ -12,21 +12,30 @@ plot(mds)
 plot(mds, type = 'n')
 text(mds[, 1], mds[, 2])#, labels(master))
 
-# Plot the labs first row's color spots with their field number labels into a 2D space
-lab <- as.matrix(lab)[1,3:194]
-lab <- t(lab)
-lab <- as.data.frame(matrix(unlist(lab, use.names=FALSE),ncol=3, byrow=TRUE))
-colnames(lab) <- c("L", "a", "b")
-lab <- as.matrix(lab)
-mds.l <- cmdscale(dist(lab))
-plot(mds.l)
-plot(mds.l, type = 'n')
-text(mds.l[, 1], mds.l[, 2], col="red")
 
-# Plot both (master and first lab result) into one space
-plot(mds.l, type = 'n', main="Master and Lab Result compared")
+# Plot the labs means of all color spots with their field number labels into a 2D space
+# calculate the lab measurements means
+mlab <- colMeans(x = lab)
+mlab <- as.matrix(mlab)[3:194]
+mlab <- t(mlab)
+mlab <- as.data.frame(matrix(unlist(mlab, use.names=FALSE),ncol=3, byrow=TRUE))
+colnames(mlab) <- c("L", "a", "b")
+mlab <- as.matrix(mlab)
+mds.ml <- cmdscale(dist(mlab))
+plot(mds.ml)
+plot(mds.ml, type = 'n')
+text(mds.ml[, 1], mds.ml[, 2], col="red")
+
+
+# Plot both (master and mean lab results) into one space
+plot(mds.ml, type = 'n', main="Master and Mean Lab Results compared")
 text(mds[, 1], mds[, 2], cex=0.8)
-text(mds.l[, 1], mds.l[, 2], col="red", cex=0.8)
+text(mds.ml[, 1], mds.ml[, 2], col="red", cex=0.8)
+legend(x = 40 , y = 51,
+       legend = c("Master", "Mean Lab Results"), 
+       fill = c("black", "red"), 
+       bty = "n", 
+       cex = 0.8)
 
 # add connecting lines into plot
-segments(mds[, 1], mds[, 2], mds.l[, 1], mds.l[, 2] ,col = par("fg"), lty = par("lty"), lwd = par("lwd"))
+segments(mds[, 1], mds[, 2], mds.ml[, 1], mds.ml[, 2] ,col = par("fg"), lty = par("lty"), lwd = par("lwd"))
