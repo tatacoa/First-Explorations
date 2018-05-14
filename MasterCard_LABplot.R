3f7222338066a578ba1e1c1e3ab74af78a48b319
binsi <binasteinert@hotmail.de>
2018-05-14 17:49:44 +0200

    Update and rename FirstProject.R to MasterCard_LABplot.R

--- MasterCard_LABplot.R
+++ MasterCard_LABplot.R
@@ -4,36 +4,33 @@
 lab.measure <- read.csv2("FILENAME.csv")
 master.color <- read.csv2("FILENAME2") # CMYKS colors are in p1- p5 (neglect?)
 
-## Play with data
 # install.packages("plot3D")
 # install.packages("rgl")
+# install.packages("scatterplot3d")
+library("scatterplot3d")
+library(colorspace)
 library("plot3D")
 library("rgl")
 
-plot(master.color)
+plot(master.color[1:3])
+plot3d(master.color, col=4)
 
-plot3d(master.color$Field, master.color$Crow, master.color$Ccol, 
-       col=4)
-
-plot(lab.measure$Row, lab.measure$Column)
-# install.packages("scatterplot3d") # install
-library("scatterplot3d") # load
-library(colorspace) # load
-
-# save L, a and b values from dataframe to variables
+# save L, a and b values from dataframe to vectors
 Lstar = master.color$L
 Astar = master.color$a
 Bstar = master.color$b
-# apply unknown magic
+
+# create new dataframe from L-a-b vectors
 solarized = structure(list(Lstar, Astar, Bstar), .Names = c("Lstar", "Astar", "Bstar"), row.names = c(NA,16), class = "data.frame")
 head(solarized) # show top
 solarizedLAB = with(solarized, LAB(Lstar, Astar, Bstar))
 
+# show us all 64 color spots in a L-a-b plot coloured in their LAB color
 scatterplot3d(master.color[,9:11], 
-              main="3D Scatter Plot",
-              color = hex(solarizedLAB, fixup = TRUE), 
+              main="Master Color Card - Lab values",
+              color = hex(solarizedLAB, fixup = TRUE),
               pch = 16, 
               type = "h",
               grid=TRUE,
-              col.grid = "lightpink",
+              #col.grid = "lightblue",
               box=FALSE)
