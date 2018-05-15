9e554d6e6052e639422c91137d19c88952e2d1b3
Jannis Busch <jabusch24@googlemail.com>
2018-05-15 18:08:21 +0200

    Update CIE94.R
    added column names to lab.distances

--- CIE94.R
+++ CIE94.R
@@ -68,4 +68,5 @@ for (m in c(1:(sheets*42))){
   lab.distances[l, 1] <- lab.measure[l, 1]
   lab.distances[l, 2] <- lab.measure[l, 2]
 }
+colnames(lab.distances) <- colnames(deltae) <- c("Row", "Column", c(11:18, 21:28, 31:38, 41:48, 51:58, 61:68, 71:78, 81:88))
 save(lab.distances, file="deltae94.Rda")
